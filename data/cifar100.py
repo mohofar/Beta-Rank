415a41efc799164a92f96c496d7d49f96a829781
Morteza Homayounfar <homayounfar.m@yahoo.com>
2023-02-27 18:22:43 +0800

    Add files via upload

---
+++ data/cifar100.py
@@ -0,0 +1,48 @@
+
+import torch
+import torch.utils
+import torch.utils.data.distributed
+
+import torchvision
+from torchvision import datasets, transforms
+
+def load_data(args):
+
+    # load training data
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    trainset = torchvision.datasets.CIFAR10(root=args.data_dir, train=True, download=True,
+                                            transform=transform_train)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root=args.data_dir, train=False, download=True, transform=transform_test)
+    val_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+
+    return train_loader, val_loader
+
+
+    CIFAR100_TRAIN_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
+    CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
+    
+    train_loader = get_training_dataloader(
+        CIFAR100_TRAIN_MEAN,
+        CIFAR100_TRAIN_STD,
+        num_workers=4,
+        batch_size=args.b,
+        shuffle=True
+    )
+
+    val_loader = get_test_dataloader(
+        CIFAR100_TRAIN_MEAN,
+        CIFAR100_TRAIN_STD,
+        num_workers=4,
+        batch_size=args.b,
+        shuffle=True
+    )
